282fff61e5fdb6330479c1aa7afedc965a95334c
ramirouvalle <ramiro.uvallevgr@uanl.edu.mx>
2017-05-29 16:27:35 -0500

    guardando

---
+++ src/java/beans/Administrator.java
@@ -0,0 +1,55 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Administrator {
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+    public Administrator() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" + "userName=" + userName + ", password=" + password + ", firstName=" + firstName + ", lastName=" + lastName + '}';
+    }
+    
+    
+}
